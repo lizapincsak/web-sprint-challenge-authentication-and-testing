69f486e635f9bd0757bcf67999f888528b0e61d8
Liza Pincsak <lizapincsak@MacBook-Air-2.local>
2021-04-23 11:30:11 -0500

    wip

--- api/server.test.js
+++ api/server.test.js
@@ -1,4 +1,27 @@
-// Write your tests here
+const request = require("supertest");
+const db = require("../data/dbConfig.js");
+const server = require('../api/server.js');
+
+const User = require("./users/users-model");
+
+const bob = {name: "Bob"}
+const mary = {name: "Mary"}
+
+beforeAll(async ()=>{
+  await db.migrate.rollback()
+  await db.migrate.latest()
+})
+beforeEach(async ()=>{
+  await db("users").truncate()
+})
+afterAll(async ()=>{
+  await db.destroy()
+})
+
 test('sanity', () => {
   expect(true).toBe(false)
+})
+
+it("correct env", ()=>{
+  expect(process.env.DB_ENV).toBe("testing")
 })
